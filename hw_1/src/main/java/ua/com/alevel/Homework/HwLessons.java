a6e01d5c6568ca0f8910cfe6b10118567bdcdf41
Barakudza <xianongame@gmail.com>
2024-01-29 21:13:03 +0100

    commit

--- hw_1/src/main/java/ua/com/alevel/Homework/HwLessons.java
+++ hw_1/src/main/java/ua/com/alevel/Homework/HwLessons.java
@@ -1,4 +1,20 @@
 package ua.com.alevel.Homework;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HwLessons {
+    public static void hw3() throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Type number lesson: ");
+    int lessonNumber = Integer.parseInt(reader.readLine());
+
+        int startLessons=9*60;
+        int oneLesson = 45;
+        int paired= 110;
+        int notPaired= 50;
+
+        int result = startLessons + oneLesson + (((lessonNumber-1)/2)*paired)+(((lessonNumber-1) %2)*notPaired);
+        System.out.print("Time lesson end " + result/60+":"+result%60);
+    }
 }
--- hw_1/src/main/java/ua/com/alevel/Main.java
+++ hw_1/src/main/java/ua/com/alevel/Main.java
@@ -1,11 +1,13 @@
 package ua.com.alevel;
 import ua.com.alevel.Homework.HwHowMuch;
+import ua.com.alevel.Homework.HwLessons;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         //Hw1p1.all();
-        HwHowMuch.hw2();
+        //HwHowMuch.hw2();
+        HwLessons.hw3();
         }
     }
